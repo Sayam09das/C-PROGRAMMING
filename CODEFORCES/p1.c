a8cb661f806ce34db3316255ba596297ff7b6216
Sayam Das <154095595+Sayam09das@users.noreply.github.com>
2024-02-05 22:43:00 +0530

    Add files via upload

---
+++ CODEFORCES/p1.c
@@ -0,0 +1,26 @@
+/*Write a program that takes a number N as the input, and prints it to the output.
+
+Input Format
+The only line of input contains a single integer.
+
+Output Format
+Output the answer in a single line.
+
+Constraints
+0 ≤ N ≤ 10   */
+
+/*Sample 1:
+input: 123  output: 123
+
+Sample 2:
+input: 123  output: 123  */
+
+#include <stdio.h>
+
+int main() {
+    int N;
+    printf("ENTER THE NUMBER: ");
+    scanf("%d", &N);
+    printf("THE NUMBER IS: %d\n", N);
+    return 0;
+}
